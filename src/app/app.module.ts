38b1669d334e9ffa0af3a16e066f1c112c06b2a0
sudheer <sudheergupta0508@gmail.com>
2022-01-18 07:27:34 +0530

    components are created vi ng cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ControlComponent } from './control/control.component';
+import { CountrydetailsComponent } from './countrydetails/countrydetails.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ControlComponent,
+    CountrydetailsComponent
   ],
   imports: [
     BrowserModule,
