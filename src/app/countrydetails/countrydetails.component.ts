26331c9eeb527f7020b592ab2d245ca9cc71f2ea
sudheer <sudheergupta0508@gmail.com>
2022-01-18 08:25:17 +0530

    Component details

--- src/app/countrydetails/countrydetails.component.ts
+++ src/app/countrydetails/countrydetails.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Store } from '@ngrx/store';
+import { take } from 'rxjs/operators';
+import { Country } from '../../model/country.model';
+import { countriesSelector } from '../../store/region.selector';
 @Component({
   selector: 'app-countrydetails',
   templateUrl: './countrydetails.component.html',
-  styleUrls: ['./countrydetails.component.css']
+  styleUrls: ['./countrydetails.component.css'],
 })
 export class CountrydetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  selectedCountry: Country | undefined = undefined;
+  constructor(public store: Store) {}
+  loadCountry() {
+    let localStore: any = {};
+    this.store.pipe(take(1)).subscribe((s) => (localStore = s));
+    if (localStore.state?.selectedCountry) {
+      this.selectedCountry = localStore.state?.selectedCountry;
+    }
   }
-
+  ngOnInit(): void {}
 }
